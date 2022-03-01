3db416103c19a9212f3f38a1d19a1280d443aa0a
Nicolas Eloy <colasrsca@gmail.com>
2022-03-01 14:39:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# forkify Project
+
+Recipe application with custom recipe uploads.
+
+Udemy project : The Complete JavaScript Course 2022: From Zero to Expert!
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8aaa5303-47a1-4fc8-97f7-8b9b929cf35a';
+export const MODAL_CLOSE_SEC = 2.5;
